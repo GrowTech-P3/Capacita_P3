3609623fdd661eb7cccffefb00191a482cd43c87
Josemilson Júnior <junior.tn1@gmail.com>
2021-05-23 11:34:59 -0300

    Inclusao de autenticacao por token na rota instituicao  * autenticacao incluido nas rotas usando middleware    authentication

--- src/routes/instituicaoRoutes.js
+++ src/routes/instituicaoRoutes.js
@@ -1,8 +1,9 @@
 module.exports = function (app) {
     const instituicao = require('../controller/instituicaoController.js')
+    const {authenticate} = require("../middlewares/authToken");
     app.route('/instituicao')
-        .get(instituicao.listAll)
+        .get(authenticate,instituicao.listAll)
         .post(instituicao.createOne)
     app.route('/instituicao-pesquisa')
-        .post(instituicao.searchAll)
+        .post(authenticate,instituicao.searchAll)
 }
